0e4afc42818b73b151cfb077fe3512dde9a561ad
giorgiameffe <giorgiameffe03@gmail.com>
2025-04-03 17:48:06 +0200

    feat: add google fonts and css header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,18 +6,21 @@ import logo from '../assets/img/dc-logo.png';
 export default function Header() {
     return (
         <header>
-            <img src={logo} alt="Logo-dc" />
-            <ul>
-                <li>Characters</li>
-                <li>Comics</li>
-                <li>Movies</li>
-                <li>Tv</li>
-                <li>Games</li>
-                <li>Collectibles</li>
-                <li>Videos</li>
-                <li>News</li>
-                <li>Shop</li>
-            </ul>
+            <nav className='pl-pd'>
+                <img className='dc-logo' src={logo} alt="Logo-dc" />
+                <ul className='menu-list'>
+                    <li>Characters</li>
+                    <li>Comics</li>
+                    <li>Movies</li>
+                    <li>Tv</li>
+                    <li>Games</li>
+                    <li>Collectibles</li>
+                    <li>Videos</li>
+                    <li>Fans</li>
+                    <li>News</li>
+                    <li>Shop</li>
+                </ul>
+            </nav>
         </header>
     );
 }
