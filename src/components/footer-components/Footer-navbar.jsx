376b8b5c360cd9c380f709fa5ac23ba040820e51
giorgiameffe <giorgiameffe03@gmail.com>
2025-04-03 19:48:49 +0200

    feat: complete footer navbar css

--- src/components/footer-components/Footer-navbar.jsx
+++ src/components/footer-components/Footer-navbar.jsx
@@ -1,28 +1,37 @@
+// importazione immagini
+import ComicsDigital from '../../assets/img/buy-comics-digital-comics.png';
+import Merchandise from '../../assets/img/buy-comics-merchandise.png';
+import ShopLocator from '../../assets/img/buy-comics-shop-locator.png';
+import Subscription from '../../assets/img/buy-comics-subscriptions.png';
+import PowerVisa from '../../assets/img/buy-dc-power-visa.svg';
+
+
+
 // Funzione Footer Navbar
 
 export default function FooterNavbar() {
     return (
-        <nav>
+        <nav className='nav-element'>
             <ul className="f-navbar">
                 <li>
-                    <img src="" alt="" />
-                    <div>Digital Comics</div>
+                    <img className='f-images' src={ComicsDigital} alt="Digital Comics" />
+                    <div className='navbar-text'>Digital Comics</div>
                 </li>
                 <li>
-                    <img src="" alt="" />
-                    <div>Dc Merchandise</div>
+                    <img className='f-images' src={Merchandise} alt="" />
+                    <div className='navbar-text'>Dc Merchandise</div>
                 </li>
                 <li>
-                    <img src="" alt="" />
-                    <div>Subscription</div>
+                    <img className='f-images' src={Subscription} alt="" />
+                    <div className='navbar-text'>Subscription</div>
                 </li>
                 <li>
-                    <img src="" alt="" />
-                    <div>Comic Shop Locator</div>
+                    <img className='f-images' src={ShopLocator} alt="" />
+                    <div className='navbar-text'>Comic Shop Locator</div>
                 </li>
                 <li>
-                    <img src="" alt="" />
-                    <div>Dc Power Visa</div>
+                    <img className='f-images' src={PowerVisa} alt="" />
+                    <div className='navbar-text'>Dc Power Visa</div>
                 </li>
             </ul>
         </nav>
