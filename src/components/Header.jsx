779794619b89140353aa64e456971f1a6bc2ccd8
giorgiameffe <giorgiameffe03@gmail.com>
2025-04-03 17:11:38 +0200

    feat: create header structure

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,24 @@
+// Logo
+import logo from '../assets/img/dc-logo.png';
+
 // Funzione Header 
 
 export default function Header() {
-    return (<header>Header</header>);
+    return (
+        <header>
+            <img src={logo} alt="Logo-dc" />
+            <ul>
+                <li>Characters</li>
+                <li>Comics</li>
+                <li>Movies</li>
+                <li>Tv</li>
+                <li>Games</li>
+                <li>Collectibles</li>
+                <li>Videos</li>
+                <li>News</li>
+                <li>Shop</li>
+            </ul>
+        </header>
+    );
 }
 
